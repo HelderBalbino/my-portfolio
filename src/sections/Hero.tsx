f295dbc002abc0ec5cd5273cf2ca0f20351b0d8f
Helder Balbino <helderbalbino@googlemail.com>
2025-02-13 22:44:41 +0000

    feat: enhance HeroSection with project description and call to action

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,10 +6,16 @@ export const HeroSection = () => {
 		<div>
 			<div className='container'>
 				<Image src={memojiImage} alt='Memoji' />
-			</div>
-			<div>
-				<div></div>
-				<div>Available for new projects</div>
+				<div>
+					<div></div>
+					<div>Available for new projects</div>
+				</div>
+				<h1>Building exceptional user experiences</h1>
+				<p>
+					I specialize in transforming designs into functional, high
+					performing websites and web applications. Let's discuss your
+					project and see how I can help.
+				</p>
 			</div>
 		</div>
 	);
