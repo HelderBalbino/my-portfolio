0dc1cda2ce9f42d4c3c89437500e694d57dbce89
Helder Balbino <helderbalbino@googlemail.com>
2025-02-13 21:58:08 +0000

    feat: update HeroSection to use new memoji image and add project availability message

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,16 @@
-import memojiImage from '@/assets/images/memoji.png';
+import memojiImage from '@/assets/images/memoji-computer.png';
+import Image from 'next/image';
 
 export const HeroSection = () => {
 	return (
 		<div>
-			<div className='container'></div>
+			<div className='container'>
+				<Image src={memojiImage} alt='Memoji' />
+			</div>
+			<div>
+				<div></div>
+				<div>Available for new projects</div>
+			</div>
 		</div>
 	);
 };
