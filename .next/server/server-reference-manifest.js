e1098467df8a3404acbc2cda13a008c0a28eca60
Helder Balbino <helderbalbino@googlemail.com>
2025-02-19 18:35:57 +0000

    feat: clean up manifest files by removing unused entries and updating encryption key

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aqzIPppviClRPmzVE9dT1+O2KZS2hlOgxHQqcBJYN2g=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"duqvKeKZLViPNZrdcv0ezZ8cCZLDKnOxmrrbYTNTXL4=\"\n}"
