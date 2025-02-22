900acc480d14b41d354472f885757f8487edddba
Helder Balbino <helderbalbino@googlemail.com>
2025-02-22 18:50:52 +0000

    feat: update app-paths manifest and enhance HeroSection button styling for improved layout and usability

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -27,8 +27,8 @@ export const HeroSection = () => {
 					performing websites and web applications. Let's discuss your
 					project and see how I can help.
 				</p>
-				<div className='flex flex-col'>
-					<button className='inline-flex items-center gap-2 mt-8'>
+				<div className='flex flex-col items-center'>
+					<button className='inline-flex items-center gap-2 border border-white/15'>
 						<span>Explore my work</span>
 						<ArrowDown className='size-4' />
 					</button>
