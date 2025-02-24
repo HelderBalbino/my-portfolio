eb17dbd18677946ba60547a652d6c675fd7cd977
Helder Balbino <helderbalbino@googlemail.com>
2025-02-24 23:58:42 +0000

    feat: center align button container in HeroSection for improved layout

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -29,7 +29,7 @@ export const HeroSection = () => {
 						discuss your project and see how I can help.
 					</p>
 				</div>
-				<div className='flex flex-col md:flex-row items-center mt-8 gap-4'>
+				<div className='flex flex-col md:flex-row  justify-center items-center mt-8 gap-4'>
 					<button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
 						<span className='font-semibold'>Explore my work</span>
 						<ArrowDown className='size-4' />
