12bc62e80f77d97a11bea74d8cc1cea453bace9f
Helder Balbino <helderbalbino@googlemail.com>
2025-02-24 23:52:50 +0000

    feat: increase padding in HeroSection for improved layout and readability

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
 	return (
-		<div className='py-20'>
+		<div className='py-32 md:py-48'>
 			<div className='container'>
 				<div className='flex flex-col items-center'>
 					<Image
@@ -19,14 +19,16 @@ export const HeroSection = () => {
 						</div>
 					</div>
 				</div>
-				<h1 className='font-serif text-3xl text-center mt-8 tracking-wide'>
-					Building exceptional user experiences
-				</h1>
-				<p className='mt-4 text-center text-white/60'>
-					I specialize in transforming designs into functional, high
-					performing websites and web applications. Let's discuss your
-					project and see how I can help.
-				</p>
+				<div className='max-w-lg mx-auto'>
+					<h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>
+						Building exceptional user experiences
+					</h1>
+					<p className='mt-4 text-center text-white/60'>
+						I specialize in transforming designs into functional,
+						high performing websites and web applications. Let's
+						discuss your project and see how I can help.
+					</p>
+				</div>
 				<div className='flex flex-col items-center mt-8 gap-4'>
 					<button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
 						<span className='font-semibold'>Explore my work</span>
