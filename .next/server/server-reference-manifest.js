27418a0f151d957ce274c86e5980d585f2beb621
Helder Balbino <helderbalbino@googlemail.com>
2025-03-01 16:48:26 +0000

    feat: update build and server manifests for improved routing and performance

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3GahYJqAb6fumRIa37H1JmozYnWydAM4tlHTksRBi9U=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UWfD5cDXBhqRvVGMUPPjO9Myn/ikTomIHOkJsqU3atU=\"\n}"
