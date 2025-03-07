48d8c2969772023bd1337a5ff40d82eacf03d761
Helder Balbino <helderbalbino@googlemail.com>
2025-03-07 22:26:39 +0000

    chore: update webpack cache files and modify app paths manifest for improved asset management

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UWfD5cDXBhqRvVGMUPPjO9Myn/ikTomIHOkJsqU3atU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Eu6dqFy9cqbkyuMnjKKkB1bLINKBZR4p2LOQtb17qHk=\"\n}"
