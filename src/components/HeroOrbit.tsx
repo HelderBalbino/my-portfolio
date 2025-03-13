7054d9e452c901a6043fc71e486513f5bad963f6
Helder Balbino <helderbalbino@googlemail.com>
2025-03-13 01:16:26 +0000

    chore: update HeroOrbit component to include rotation prop for enhanced star animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -3,14 +3,22 @@ import { PropsWithChildren } from 'react';
 export const HeroOrbit = ({
 	children,
 	size,
-}: PropsWithChildren<{ size: number }>) => {
+	rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
 	return (
 		<div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
 			<div
-				className='border border-red-500'
-				style={{ height: `${size}px`, width: `${size}px` }}
+				className=''
+				style={{
+					transform: `rotate(${rotation}deg)`,
+					height: `${size}px`,
+					width: `${size}px`,
+				}}
 			>
-				<div className='border border-red-500 inline-flex'>
+				<div
+					className='inline-flex'
+					style={{ transform: `rotate(${rotation * -1}deg)` }}
+				>
 					{children}
 				</div>
 			</div>
