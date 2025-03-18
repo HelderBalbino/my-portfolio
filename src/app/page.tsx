998f6ac8a4834a0b3896f0a8a9259aa0ec9c62c5
Helder Balbino <helderbalbino@googlemail.com>
2025-03-18 13:55:52 +0000

    chore: add ProjectsSection import to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/sections/Header';
 import { HeroSection } from '@/sections/Hero';
+import { ProjectsSection } from '@/sections/Projects';
 
 export default function Home() {
 	return (
