018193fcd7e334a682dfa6492e46733506aaff69
Helder Balbino <helderbalbino@googlemail.com>
2025-03-24 20:37:10 +0000

    fix: update app paths and build manifests for accuracy and optimization

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/AYGsG/iakP3N2hXZObpsm0bgLGER/IR72cR1VHJxw0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hNSlvhB6z0IZHSinhdoMmArBCIrPalYkDxR1XCzuVOQ=\"\n}"
