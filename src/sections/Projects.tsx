b6d71ae35eb46579369397e5045e6bd1d5a8263c
Helder Balbino <helderbalbino@googlemail.com>
2025-03-25 13:38:27 +0000

    fix: enhance project item styling with pseudo-element for improved layout

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -62,7 +62,7 @@ export const ProjectsSection = () => {
 					{portfolioProjects.map((project) => (
 						<div
 							key={project.title}
-							className='bg-gray-800 rounded-3xl relative'
+							className='bg-gray-800 rounded-3xl relative after:content-[""] after:absolute after:inset-0'
 						>
 							<div>
 								<span>{project.company}</span>
