b0cd3b4d9c6a9df7b5eb011fd17e0c78707bc624
Helder Balbino <helderbalbino@googlemail.com>
2025-03-26 01:19:26 +0000

    fix: update project item layout with gap adjustment and improved text styling

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -65,8 +65,9 @@ export const ProjectsSection = () => {
 							className='bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[""] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20'
 						>
 							<div className='flex'>
-								<div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-center text-transparent bg-clip-text inline-flex font-bold uppercase tracking-widest text-sm'>
+								<div className='bg-gradient-to-r from-emerald-300 to-sky-400  inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text'>
 									<span>{project.company}</span>
+									<span>&bull;</span>
 									<span>{project.year}</span>
 								</div>
 							</div>
