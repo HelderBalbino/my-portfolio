3ecddbd7d96b20875bd446cefe170b3a27f4a613
Helder Balbino <helderbalbino@googlemail.com>
2025-03-26 14:05:19 +0000

    fix: add check circle icon to project results and adjust margin for improved spacing

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,7 @@ import darkSaasLandingPage from '@/assets/images/dark-saas-landing-page.png';
 import lightSaasLandingPage from '@/assets/images/light-saas-landing-page.png';
 import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png';
 import Image from 'next/image';
+import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 
 const portfolioProjects = [
 	{
@@ -74,10 +75,13 @@ export const ProjectsSection = () => {
 							<h3 className='font-serif text-2xl mt-2'>
 								{project.title}
 							</h3>
-							<hr className='border-t-2 border-white/5 mt-4' />
+							<hr className='border-t-2 border-white/5 mt-9' />
 							<ul>
 								{project.results.map((result) => (
-									<li>{result.title}</li>
+									<span>
+										<CheckCircleIcon className='w-5 h-5 text-emerald-300 inline' />
+										<li>{result.title}</li>
+									</span>
 								))}
 							</ul>
 							<a href={project.link}>
