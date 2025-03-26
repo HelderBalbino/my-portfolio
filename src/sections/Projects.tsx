26276978abf7f44bc2a4eec2cfc08e86d8777e7c
Helder Balbino <helderbalbino@googlemail.com>
2025-03-26 01:10:28 +0000

    fix: update project item layout with flexbox and gradient text styling

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -64,8 +64,8 @@ export const ProjectsSection = () => {
 							key={project.title}
 							className='bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[""] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20'
 						>
-							<div>
-								<div>
+							<div className='flex'>
+								<div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-center text-transparent bg-clip-text'>
 									<span>{project.company}</span>
 									<span>{project.year}</span>
 								</div>
