1742c45f22af1f08bd9bccfc72b0aad75cfa5cae
Helder Balbino <helderbalbino@googlemail.com>
2025-03-27 14:14:20 +0000

    fix: update button styling in Projects section for improved appearance

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -86,7 +86,7 @@ export const ProjectsSection = () => {
 								))}
 							</ul>
 							<a href={project.link}>
-								<button className='bg-white text-green-900 h-12 w-full rounded-full font-bold'>
+								<button className='bg-white text-green-900 h-12 w-full rounded-full font-semibold inline-flex'>
 									<span>View Live Site</span>
 									<ArrowUpRightIcon className='size-4' />
 								</button>
