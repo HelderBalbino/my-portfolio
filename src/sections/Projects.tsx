ab5b6cc8c0b88018f04649686cfd041342c4d376
Helder Balbino <helderbalbino@googlemail.com>
2025-03-31 15:02:48 +0100

    fix: adjust margin for horizontal rule in Projects section on medium screens

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -83,7 +83,7 @@ export const ProjectsSection = () => {
 							<h3 className='font-serif text-2xl mt-2 md:mt-5 md:text-4xl'>
 								{project.title}
 							</h3>
-							<hr className='border-t-2 border-white/5 mt-6' />
+							<hr className='border-t-2 border-white/5 mt-4 md:mt-5' />
 							<ul className='flex flex-col gap-4 mt-4'>
 								{project.results.map((result) => (
 									<li className='flex gap-2 text-sm text-white/50'>
