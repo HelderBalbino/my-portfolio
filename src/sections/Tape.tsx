5a1ca039dfd3c3f5baa6d13e9cf9e6924e4155a2
Helder Balbino <helderbalbino@googlemail.com>
2025-04-03 12:12:51 +0100

    style: add padding to TapeSection and adjust StarIcon color for improved layout

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -18,7 +18,7 @@ export const TapeSection = () => {
 		<div className='py-16'>
 			<div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip'>
 				<div className='flex'>
-					<div className='flex flex-none gap-4'>
+					<div className='flex flex-none gap-4 py-3'>
 						{words.map((word) => (
 							<div
 								key={word}
@@ -27,7 +27,7 @@ export const TapeSection = () => {
 								<span className='text-gray-900 uppercase font-extrabold text-sm'>
 									{word}
 								</span>
-								<StarIcon className='size-6' />
+								<StarIcon className='size-6 text-gray-900' />
 							</div>
 						))}
 					</div>
