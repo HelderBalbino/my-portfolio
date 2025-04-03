c1992ba43bd9c75f4d8d8746923c9e4e7be5f6ac
Helder Balbino <helderbalbino@googlemail.com>
2025-04-03 12:04:44 +0100

    style: update TapeSection layout with gradient background and improved flex structure

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -16,13 +16,17 @@ const words = [
 export const TapeSection = () => {
 	return (
 		<div className='py-16'>
-			<div className='flex gap-4'>
-				{words.map((word) => (
-					<div key={word} className='inline-flex gap-4'>
-						<span>{word}</span>
-						<StarIcon className='size-6' />
+			<div className='bg-gradient-to-r from-emerald-300 to-sky-400'>
+				<div className='flex'>
+					<div className='flex flex-none gap-4'>
+						{words.map((word) => (
+							<div key={word} className='inline-flex gap-4'>
+								<span>{word}</span>
+								<StarIcon className='size-6' />
+							</div>
+						))}
 					</div>
-				))}
+				</div>
 			</div>
 		</div>
 	);
