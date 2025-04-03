215d58cfaade339f396d9be0ed3f6516f1478212
Helder Balbino <helderbalbino@googlemail.com>
2025-04-03 11:36:23 +0100

    feat: add words array to TapeSection for enhanced content variety

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,16 @@
+const words = [
+	'Performant',
+	'Accessible',
+	'Secure',
+	'Interactive',
+	'Scalable',
+	'User-friendly',
+	'Maintainable',
+	'Search Optimized',
+	'Usable',
+	'Reliable',
+];
+
 export const TapeSection = () => {
 	return <div>Tape Section</div>;
 };
