0054c24c238de286f38cb3117f7fbbd742af922d
Helder Balbino <helderbalbino@googlemail.com>
2025-04-03 12:10:00 +0100

    style: enhance TapeSection layout with overflow handling and improved word styling

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -16,12 +16,17 @@ const words = [
 export const TapeSection = () => {
 	return (
 		<div className='py-16'>
-			<div className='bg-gradient-to-r from-emerald-300 to-sky-400'>
+			<div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip'>
 				<div className='flex'>
 					<div className='flex flex-none gap-4'>
 						{words.map((word) => (
-							<div key={word} className='inline-flex gap-4'>
-								<span>{word}</span>
+							<div
+								key={word}
+								className='inline-flex gap-4 items-center'
+							>
+								<span className='text-gray-900 uppercase font-extrabold text-sm'>
+									{word}
+								</span>
 								<StarIcon className='size-6' />
 							</div>
 						))}
