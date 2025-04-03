22840fdd1b8d0a3a3d42d972e60e3c625195f7f6
Helder Balbino <helderbalbino@googlemail.com>
2025-04-03 12:26:12 +0100

    style: enhance TapeSection layout with improved padding and rotation effects

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -15,8 +15,8 @@ const words = [
 
 export const TapeSection = () => {
 	return (
-		<div className='py-16'>
-			<div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip'>
+		<div className='py-16 lg:py-24'>
+			<div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1'>
 				<div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
 					<div className='flex flex-none gap-4 py-3'>
 						{words.map((word) => (
@@ -27,7 +27,7 @@ export const TapeSection = () => {
 								<span className='text-gray-900 uppercase font-extrabold text-sm'>
 									{word}
 								</span>
-								<StarIcon className='size-6 text-gray-900' />
+								<StarIcon className='size-6 text-gray-900 -rotate-12' />
 							</div>
 						))}
 					</div>
