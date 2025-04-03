a9671664fa695d0ffa8264328769782782e3be1f
Helder Balbino <helderbalbino@googlemail.com>
2025-04-03 11:55:06 +0100

    style: enhance layout of words in TapeSection for improved visual structure

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -16,12 +16,14 @@ const words = [
 export const TapeSection = () => {
 	return (
 		<div>
-			{words.map((word) => (
-				<div key={word}>
-					<span>{word}</span>
-					<StarIcon />
-				</div>
-			))}
+			<div className='flex gap-4'>
+				{words.map((word) => (
+					<div key={word} className='inline-flex gap-4'>
+						<span>{word}</span>
+						<StarIcon />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
