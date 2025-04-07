572a005e78b8e7f7f505de5a86fe9fcdafd45fd1
Helder Balbino <helderbalbino@googlemail.com>
2025-04-07 23:45:41 +0100

    style: implement rendering logic in SectionHeader component

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -7,5 +7,19 @@ export const SectionHeader = ({
 	eyebrow: string;
 	description: string;
 }) => {
-	return <></>;
+	return (
+		<>
+			<div className='flex justify-center'>
+				<p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center text-transparent bg-clip-text'>
+					Real-world Results
+				</p>
+			</div>
+			<h2 className='font-serif text-3xl md:text-5xl text-center mt-7'>
+				Featured Projects
+			</h2>
+			<p className='text-center md:text-lg lg:text-xl text-white/60 mt-6 max-w-md mx-auto'>
+				See how I transformed concepts into engaging digital experiences
+			</p>
+		</>
+	);
 };
