1c4f48876980553de0ed5127170816d0d5a7c597
Helder Balbino <helderbalbino@googlemail.com>
2025-04-07 23:47:50 +0100

    style: update SectionHeader component to use props for eyebrow, title, and description

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -11,14 +11,14 @@ export const SectionHeader = ({
 		<>
 			<div className='flex justify-center'>
 				<p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center text-transparent bg-clip-text'>
-					Real-world Results
+					{eyebrow}
 				</p>
 			</div>
 			<h2 className='font-serif text-3xl md:text-5xl text-center mt-7'>
-				Featured Projects
+				{title}
 			</h2>
 			<p className='text-center md:text-lg lg:text-xl text-white/60 mt-6 max-w-md mx-auto'>
-				See how I transformed concepts into engaging digital experiences
+				{description}
 			</p>
 		</>
 	);
