d8fa53a83c87cbd0a6743cd748ef9a57119c0699
Helder Balbino <helderbalbino@googlemail.com>
2025-05-19 17:07:28 +0100

    style: update testimonial names for accuracy and remove unused memoji avatar

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -2,7 +2,6 @@ import memojiAvatar1 from '@/assets/images/memoji-avatar-1.png';
 import memojiAvatar2 from '@/assets/images/memoji-avatar-2.png';
 import memojiAvatar3 from '@/assets/images/memoji-avatar-3.png';
 import memojiAvatar4 from '@/assets/images/memoji-avatar-4.png';
-import memojiAvatar5 from '@/assets/images/memoji-avatar-5.png';
 import { SectionHeader } from '@/components/SectionHeader';
 import Image from 'next/image';
 import grainImage from '@/assets/images/grain.jpg';
@@ -15,7 +14,7 @@ const testimonials = [
 		avatar: memojiAvatar1,
 	},
 	{
-		name: 'Ramsey Ediku',
+		name: 'Jo Pickering',
 		position: 'Head of Design at GreenLeaf',
 		text: 'Working with Helder was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.',
 		avatar: memojiAvatar2,
@@ -27,16 +26,10 @@ const testimonials = [
 		avatar: memojiAvatar3,
 	},
 	{
-		name: 'Emily Carter',
+		name: 'Ramsey Ediku',
 		position: 'Product Manager @ GlobalTech',
 		text: "Helder is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
 		avatar: memojiAvatar4,
-	},
-	{
-		name: 'Michael Brown',
-		position: 'Director of IT at google',
-		text: "Helder's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
-		avatar: memojiAvatar5,
 	},
 ];
 
