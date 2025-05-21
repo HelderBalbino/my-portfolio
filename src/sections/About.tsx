79129db980312324a1915cb43e9a4be2d2ad9ad4
Helder Balbino <helderbalbino@googlemail.com>
2025-05-21 12:44:07 +0100

    style: update AboutSection to include SectionHeader with detailed description

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,13 @@
+import { SectionHeader } from '@/components/SectionHeader';
+
 export const AboutSection = () => {
-	return <div>About me Section</div>;
+	return (
+		<div>
+			<SectionHeader
+				eyebrow='About Me'
+				title='A Glimpse Into My World'
+				description='Learn more about who I am'
+			/>
+		</div>
+	);
 };
