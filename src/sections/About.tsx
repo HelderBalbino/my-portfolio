9bc454e313af26862b4a96ea18d7ba45059d487b
Helder Balbino <helderbalbino@googlemail.com>
2025-05-23 23:48:20 +0100

    style: update layout of My Reads section for improved spacing and readability

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -49,14 +49,14 @@ export const AboutSection = () => {
 				/>
 				<div className='mt-20'>
 					<Card>
-						<div>
+						<div className='flex flex-col'>
 							<div className='inline-flex items-center gap-2'>
 								<StarIcon className='size-9 text-emerald-300' />
 								<h3 className='font-serif text-3xl'>
 									My Reads
 								</h3>
 							</div>
-							<p className='text-sm text-white/60'>
+							<p className='text-sm text-white/60 mt-2'>
 								Explore the books shaping my perspectives
 							</p>
 						</div>
