96ef60c2da3bc9f049d590adda5fdb8bab420415
Helder Balbino <helderbalbino@googlemail.com>
2025-05-23 23:58:01 +0100

    fix: correct syntax error in CardHeader component definition

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,3 +1,13 @@
-export cosnt CardHeader = ( ) => {
-    return ()
-}
+export const CardHeader = () => {
+	return (
+		<div className='flex flex-col'>
+			<div className='inline-flex items-center gap-2'>
+				<StarIcon className='size-9 text-emerald-300' />
+				<h3 className='font-serif text-3xl'>My Reads</h3>
+			</div>
+			<p className='text-sm text-white/60 mt-2'>
+				Explore the books shaping my perspectives
+			</p>
+		</div>
+	);
+};
