2c6014cad9d12559c21aa0328a47a3d10a1afc8c
Helder Balbino <helderbalbino@googlemail.com>
2025-05-26 20:53:28 +0100

    fix: enhance toolbox item display with rounded outline for improved aesthetics

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -75,7 +75,7 @@ export const AboutSection = () => {
 							{toolboxItems.map((item) => (
 								<div
 									key={item.title}
-									className='inline-flex items-center gap-4 py-2 px-3 outline outline-2'
+									className='inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg'
 								>
 									<TechIcon component={item.iconType} />
 									<span className='font-semibold'>
