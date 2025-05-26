65d557d22943f55b749cb81c7941a48f2da53278
Helder Balbino <helderbalbino@googlemail.com>
2025-05-26 09:02:13 +0100

    fix: update CardHeader component to accept title and description props

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,6 +1,12 @@
 import StarIcon from '@/assets/icons/star.svg';
 
-export const CardHeader = () => {
+export const CardHeader = ({
+	title,
+	description,
+}: {
+	title: string;
+	description: string;
+}) => {
 	return (
 		<div className='flex flex-col'>
 			<div className='inline-flex items-center gap-2'>
