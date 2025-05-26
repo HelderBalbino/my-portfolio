5db6f9c3715b32561514a227e353f52a17788048
Helder Balbino <helderbalbino@googlemail.com>
2025-05-26 22:31:20 +0100

    feat: integrate twMerge for className prop in CardHeader component

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,4 +1,5 @@
 import StarIcon from '@/assets/icons/star.svg';
+import { twMerge } from 'tailwind-merge';
 
 export const CardHeader = ({
 	title,
@@ -10,7 +11,7 @@ export const CardHeader = ({
 	className?: string;
 }) => {
 	return (
-		<div className='flex flex-col'>
+		<div className={twMerge('flex flex-col', className)}>
 			<div className='inline-flex items-center gap-2'>
 				<StarIcon className='size-9 text-emerald-300' />
 				<h3 className='font-serif text-3xl'>{title}</h3>
