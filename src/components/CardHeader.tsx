d35427bd7821e3323d8cb5f5353b3fd825963cd3
Helder Balbino <helderbalbino@googlemail.com>
2025-05-26 09:03:38 +0100

    fix: update CardHeader component to use title and description props

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -11,11 +11,9 @@ export const CardHeader = ({
 		<div className='flex flex-col'>
 			<div className='inline-flex items-center gap-2'>
 				<StarIcon className='size-9 text-emerald-300' />
-				<h3 className='font-serif text-3xl'>My Reads</h3>
+				<h3 className='font-serif text-3xl'>{title}</h3>
 			</div>
-			<p className='text-sm text-white/60 mt-2'>
-				Explore the books shaping my perspectives
-			</p>
+			<p className='text-sm text-white/60 mt-2'>{description}</p>
 		</div>
 	);
 };
