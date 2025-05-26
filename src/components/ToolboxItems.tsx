0449d793c6457d5182e6531149cb7535ad6d3c2d
Helder Balbino <helderbalbino@googlemail.com>
2025-05-26 21:43:32 +0100

    refactor: restructure ToolboxItems component layout for better alignment

--- src/components/ToolboxItems.tsx
+++ src/components/ToolboxItems.tsx
@@ -9,16 +9,18 @@ export const ToolboxItems = ({
 	}[];
 }) => {
 	return (
-		<div>
-			{items.map((item) => (
-				<div
-					key={item.title}
-					className='inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg'
-				>
-					<TechIcon component={item.iconType} />
-					<span className='font-semibold'>{item.title}</span>
-				</div>
-			))}
+		<div className='flex'>
+			<div className='flex flex-none'>
+				{items.map((item) => (
+					<div
+						key={item.title}
+						className='inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg'
+					>
+						<TechIcon component={item.iconType} />
+						<span className='font-semibold'>{item.title}</span>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
