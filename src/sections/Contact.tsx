291e77673c791646593a7770d758b81512b070e6
Helder Balbino <helderbalbino@googlemail.com>
2025-06-05 13:41:16 +0100

    fix: wrap ContactSection content in a container div for better layout

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,15 +3,17 @@ import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 export const ContactSection = () => {
 	return (
 		<div>
-			<h2>Let's create something amazing together!</h2>
-			<p>
-				Whether you have a project in mind or just want to say hello,
-				I’d love to hear from you.
-			</p>
-			<button>
-				<span>Contact Me</span>
-				<ArrowUpRightIcon />
-			</button>
+			<div className='container'>
+				<h2>Let's create something amazing together!</h2>
+				<p>
+					Whether you have a project in mind or just want to say
+					hello, I’d love to hear from you.
+				</p>
+				<button>
+					<span>Contact Me</span>
+					<ArrowUpRightIcon />
+				</button>
+			</div>
 		</div>
 	);
 };
