d5773e5a3d71d88bcb887ef0c2b4a28694b42f1a
Helder Balbino <helderbalbino@googlemail.com>
2025-06-05 12:59:36 +0100

    fix: adjust padding and layout for improved responsiveness in About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -47,7 +47,7 @@ const hobbies = [
 
 export const AboutSection = () => {
 	return (
-		<div className='py-20'>
+		<div className='py-20 lg:py-28'>
 			<div className='container'>
 				<SectionHeader
 					eyebrow='About Me'
@@ -55,7 +55,7 @@ export const AboutSection = () => {
 					description='Learn more about who I am, What I do, and what Inspires me.'
 				/>
 				<div className='mt-20 flex flex-col gap-8'>
-					<div className='grid grid-cols-1 md:grid-cols-5 gap-8'>
+					<div className='grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8'>
 						<Card className='h-[320px] md:col-span-2'>
 							<CardHeader
 								title='My Reads'
