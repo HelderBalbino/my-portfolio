3163f1b0cc4735302d4dcca34de551308d313976
Helder Balbino <helderbalbino@googlemail.com>
2025-06-05 13:34:53 +0100

    fix: update ContactSection to include a welcoming message and description

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,11 @@
 export const ContactSection = () => {
-	return <div>Contact me Section</div>;
+	return (
+		<div>
+			<h2>Let's create something amazing together!</h2>
+			<p>
+				Whether you have a project in mind or just want to say hello,
+				I’d love to hear from you.
+			</p>
+		</div>
+	);
 };
