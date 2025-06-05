7d6de0e1b63c74e117c362a170af8f2a92fa7843
Helder Balbino <helderbalbino@googlemail.com>
2025-06-05 13:55:44 +0100

    fix: wrap ContactSection content in a styled div for improved layout

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,15 +4,19 @@ export const ContactSection = () => {
 	return (
 		<div className='py-16'>
 			<div className='container'>
-				<h2>Let's create something amazing together!</h2>
-				<p>
-					Whether you have a project in mind or just want to say
-					hello, I’d love to hear from you.
-				</p>
-				<button>
-					<span>Contact Me</span>
-					<ArrowUpRightIcon />
-				</button>
+				<div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center'>
+					<h2 className='font-serif text-2xl'>
+						Let's create something amazing together!
+					</h2>
+					<p className='text-sm mt-4'>
+						Whether you have a project in mind or just want to say
+						hello, I’d love to hear from you.
+					</p>
+					<button className='text-white bg-gray-900 inline-flex items-center justify-center h-12 gap-2 px-6 mt-6 rounded-xl hover:bg-gray-800 transition-colors'>
+						<span className='font-semibold'>Contact Me</span>
+						<ArrowUpRightIcon className='size-4' />
+					</button>
+				</div>
 			</div>
 		</div>
 	);
