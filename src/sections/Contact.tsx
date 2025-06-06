5d446ecbc03a032e292369f73c983d7038002751
Helder Balbino <helderbalbino@googlemail.com>
2025-06-06 02:28:04 +0100

    fix: enhance padding for improved layout in ContactSection

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,7 +3,7 @@ import grainImage from '@/assets/images/grain.jpg';
 
 export const ContactSection = () => {
 	return (
-		<div className='py-16 pt-12'>
+		<div className='py-16 pt-12 lg:py-24 lg:pt-20'>
 			<div className='container'>
 				<div className='relative z-0 overflow-hidden rounded-3xl bg-gradient-to-r from-emerald-300 to-sky-400 px-10 py-8 text-center text-gray-900 md:text-left'>
 					<div
