f95f1c3b7cb5f7e220d96e69a79c9a097ea50dc9
Helder Balbino <helderbalbino@googlemail.com>
2025-06-06 01:54:46 +0100

    fix: add z-index and opacity to background image for improved layering in ContactSection

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,9 +5,9 @@ export const ContactSection = () => {
 	return (
 		<div className='py-16 pt-14'>
 			<div className='container'>
-				<div className='relative overflow-hidden rounded-3xl bg-gradient-to-r from-emerald-300 to-sky-400 px-10 py-8 text-center text-gray-900'>
+				<div className='relative z-0 overflow-hidden rounded-3xl bg-gradient-to-r from-emerald-300 to-sky-400 px-10 py-8 text-center text-gray-900'>
 					<div
-						className='absolute inset-0'
+						className='absolute inset-0 -z-10 opacity-5'
 						style={{ backgroundImage: `url(${grainImage.src})` }}
 					></div>
 					<h2 className='font-serif text-2xl'>
