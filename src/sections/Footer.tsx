9bcc2751ce9bcbc588370f318a2c9c5e337b440a
Helder Balbino <helderbalbino@googlemail.com>
2025-06-07 22:58:14 +0100

    fix: update Footer component structure for improved layout and semantics

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,18 @@
 export const Footer = () => {
-	return <footer>My Footer Section</footer>;
+	return (
+		<footer>
+			<div className='container py-10'>
+				<div>
+					<div>
+						&copy; {new Date().getFullYear()} Helder Balbino. All
+						rights reserved.
+					</div>
+					<nav>
+						<a href=''>LinkedIn</a>
+						<a href=''>GitHub</a>
+					</nav>
+				</div>
+			</div>
+		</footer>
+	);
 };
