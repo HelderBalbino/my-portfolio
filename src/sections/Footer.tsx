67318d0900510d951194c959f232632ee9e5492b
Helder Balbino <helderbalbino@googlemail.com>
2025-06-14 09:07:56 +0100

    fix: enhance layout of Footer component for improved responsiveness

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -16,7 +16,7 @@ export const Footer = () => {
 		<footer className='relative -z-10 overflow-x-clip'>
 			<div className='absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]'></div>
 			<div className='container'>
-				<div className='flex flex-col items-center gap-8 border-t border-white/15 py-6 text-base'>
+				<div className='flex flex-col items-center gap-8 border-t border-white/15 py-6 text-base md:flex-row md:justify-between'>
 					<div className='text-white/40'>
 						&copy; {new Date().getFullYear()} Helder Balbino. All
 						rights reserved.
