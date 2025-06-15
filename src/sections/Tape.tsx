b48c6bb24ef14fc52dd70eb48227456228879f75
Helder Balbino <helderbalbino@googlemail.com>
2025-06-15 22:23:57 +0100

    fix: adjust padding in TapeSection for improved layout consistency

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -20,7 +20,7 @@ export const TapeSection = () => {
 		<div className='overflow-x-clip py-16 lg:py-24'>
 			<div className='-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400'>
 				<div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
-					<div className='flex flex-none gap-4 py-3'>
+					<div className='flex flex-none gap-4 py-3 pr-4'>
 						{[...new Array(2)].fill(0).map((_, index) => (
 							<Fragment key={index}>
 								{words.map((word) => (
