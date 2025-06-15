f81eea2f18379175260efde8b89171f3c0e9c327
Helder Balbino <helderbalbino@googlemail.com>
2025-06-15 16:31:14 +0100

    fix: reorder HeroOrbit components for improved visual hierarchy and consistency

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -21,8 +21,17 @@ export const HeroSection = () => {
 				<div className='hero-ring size-[1020px]'></div>
 				<div className='hero-ring size-[1220px]'></div>
 				{/* stars icons from heroOrbit */}
-				<HeroOrbit size={800} rotation={-72}>
-					<StarIcon className='size-28 text-emerald-300' />
+				<HeroOrbit size={430} rotation={-14}>
+					<SparkleIcon className='size-8 text-emerald-300/20' />
+				</HeroOrbit>
+				<HeroOrbit size={440} rotation={79}>
+					<SparkleIcon className='size-5 text-emerald-300/20' />
+				</HeroOrbit>
+				<HeroOrbit size={520} rotation={-41}>
+					<div className='size-2 rounded-full bg-emerald-300/20' />
+				</HeroOrbit>
+				<HeroOrbit size={530} rotation={178}>
+					<SparkleIcon className='size-10 text-emerald-300/20' />
 				</HeroOrbit>
 				<HeroOrbit size={550} rotation={20}>
 					<StarIcon className='size-12 text-emerald-300' />
@@ -30,28 +39,17 @@ export const HeroSection = () => {
 				<HeroOrbit size={590} rotation={98}>
 					<StarIcon className='size-8 text-emerald-300' />
 				</HeroOrbit>
-				{/* sparkle icons */}
-				<HeroOrbit size={430} rotation={-14}>
-					<SparkleIcon className='size-8 text-emerald-300/20' />
-				</HeroOrbit>
-				<HeroOrbit size={440} rotation={79}>
-					<SparkleIcon className='size-5 text-emerald-300/20' />
-				</HeroOrbit>
-				<HeroOrbit size={530} rotation={178}>
-					<SparkleIcon className='size-10 text-emerald-300/20' />
+				<HeroOrbit size={650} rotation={-5}>
+					<div className='size-2 rounded-full bg-emerald-300/20' />
 				</HeroOrbit>
 				<HeroOrbit size={710} rotation={144}>
 					<SparkleIcon className='size-14 text-emerald-300/20' />
 				</HeroOrbit>
-				{/* round sparkle icons */}
 				<HeroOrbit size={720} rotation={85}>
 					<div className='size-3 rounded-full bg-emerald-300/20' />
 				</HeroOrbit>
-				<HeroOrbit size={520} rotation={-41}>
-					<div className='size-2 rounded-full bg-emerald-300/20' />
-				</HeroOrbit>
-				<HeroOrbit size={650} rotation={-5}>
-					<div className='size-2 rounded-full bg-emerald-300/20' />
+				<HeroOrbit size={800} rotation={-72}>
+					<StarIcon className='size-28 text-emerald-300' />
 				</HeroOrbit>
 			</div>
 
