fd02582fda34b7303c5201652b7903a4bd9bd61f
Helder Balbino <helderbalbino@googlemail.com>
2025-06-15 16:27:54 +0100

    fix: correct className and style usage in HeroOrbit component for proper animation application

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export const HeroOrbit = ({
 	children,
@@ -18,7 +19,10 @@ export const HeroOrbit = ({
 }>) => {
 	return (
 		<div className='absolute left-1/2 top-1/2 -z-20 -translate-x-1/2 -translate-y-1/2'>
-			<div className='{twMerge(shouldOrbit === true && animate-spin)} style{{animationDuration: orbitDuration}}'>
+			<div
+				className={twMerge(shouldOrbit === true && 'animate-spin')}
+				style={{ animationDuration: orbitDuration }}
+			>
 				<div
 					className='flex items-start justify-start'
 					style={{
@@ -27,7 +31,12 @@ export const HeroOrbit = ({
 						width: `${size}px`,
 					}}
 				>
-					<div className='animate-spin [animation-duration:20s]'>
+					<div
+						className={twMerge(
+							shouldSpin === true && 'animate-spin',
+						)}
+						style={{ animationDuration: spinDuration }}
+					>
 						<div
 							className='inline-flex'
 							style={{ transform: `rotate(${rotation * -1}deg)` }}
