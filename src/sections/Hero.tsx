c3193c66c7e1365649fa5e2e4dbacf2990cddcfb
Helder Balbino <helderbalbino@googlemail.com>
2025-06-16 15:13:26 +0100

    fix: add section IDs for improved navigation and accessibility

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,10 @@ import Image from 'next/image';
 
 export const HeroSection = () => {
 	return (
-		<div className='relative z-0 overflow-x-clip py-32 md:py-48 lg:py-60'>
+		<div
+			id='home'
+			className='relative z-0 overflow-x-clip py-32 md:py-48 lg:py-60'
+		>
 			{/* adding a mask gradient  */}
 			<div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]'>
 				<div
